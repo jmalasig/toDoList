3472f62028c3d18bb6db17d8b46f0abdf06b72eb
jmalasig <joanamalasig@gmail.com>
2019-05-26 21:01:49 -0700

    Partial Iteration 1

---
+++ toDoList/AddToDoViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AddToDoViewController.swift
+//  toDoList
+//
+//  Created by Apple on 5/26/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class AddToDoViewController: UIViewController {
+    
+    var previousVC = ToDoTableViewController()
+
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+        let toDo = ToDo()
+        if let titleText = titleTextField.text {
+            toDo.name = titleText
+            toDo.important = importantSwitch.isOn
+        }
+        previousVC.toDos.append(toDo)
+        previousVC.tableView.reloadData()
+        navigationController?.popViewController(animated: true)
+    }
+    
+}
--- toDoList/ToDoTableViewController.swift
+++ toDoList/ToDoTableViewController.swift
@@ -43,10 +43,10 @@ class ToDoTableViewController: UITableViewController {
         return cell
     }
 
-    /*
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     
+        if let addVC = segue.destination as? AddToDoViewController {
+            addVC.previousVC = self
+        }
     }
-    */
 
 }
